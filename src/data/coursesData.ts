e2213079abaa0a0edc81c113346675d45c3e8096
shakeelayazjoia <shakeelayaz954@gmail.com>
2026-02-09 17:23:25 +0500

    global learning system

---
+++ src/data/coursesData.ts
@@ -0,0 +1,12 @@
+// Courses are now managed in the database
+export interface Course {
+  id: string;
+  title: string;
+  code: string;
+  category: string;
+  description: string;
+  duration: string;
+  
+}
+
+export const allCoursesData: Course[] = [];
